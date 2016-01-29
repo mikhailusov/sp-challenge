3095c90347aef8b1b2128a8ee832939fef0c8f36
Mikhail Usov <usov.me@gmail.com>
2016-01-29 15:42:00 -0500

    Initial commit

---
+++ src/main/java/com/stormpath/challenge/Application.java
@@ -0,0 +1,15 @@
+package com.stormpath.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mikhail Usov<usov.me@gmail.com> on 1/29/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
